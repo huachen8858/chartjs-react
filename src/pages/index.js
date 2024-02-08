7869c67719633cb60be1470bdbd3d769117af15b
huachen8858 <huachen8858@gmail.com>
2024-02-08 17:54:03 +0800

    chart components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import {Chart as ChartJS, defaults} from "chart.js/auto"
-import { Line } from 'react-chartjs-2'
+import LineChart from '../../components/line-chart'
+import BarChart from '../../components/bar-chart'
+import DoughnutChart from '../../components/doughnut-chart'
 
 export default function MyChart() {
   return (
     <div className='App'>
       <div className='dataCard revennueCard'>
+      <LineChart/>
       </div>
       <div className='dataCard customerCard'>
+      <BarChart/>
       </div>
       <div className='dataCard categoryCard'>
+      <DoughnutChart/>
       </div>
     </div>
   )
