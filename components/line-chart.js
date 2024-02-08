3a5f223aca8db5dcc9af00cf568ba6257d764b0d
huachen8858 <huachen8858@gmail.com>
2024-02-08 18:01:31 +0800

    line chart data map

--- components/line-chart.js
+++ components/line-chart.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Line } from "react-chartjs-2";
+import revenueData from "../src/data/revenueData.json";
 
 export default function LineChart() {
   return (
-    <div>LineChart</div>
-  )
+    <Line
+      data={{
+        labels: revenueData.map((data) => data.label),
+        datasets: [
+          {
+            label: "Revenue",
+            data: revenueData.map((data) => data.revenue),
+            backgroundColor: "#064FF0",
+            borderColor: "#064FF0"
+          },
+          {
+            label: "Cost",
+            data: revenueData.map((data) => data.cost),
+            backgroundColor: "#FF3030",
+            borderColor: "#FF3030"
+          },
+        ],
+      }}
+    />
+  );
 }
