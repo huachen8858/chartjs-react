26aeaf2c3a9fd5d6d6bf112e7f01c81c7253b395
huachen8858 <huachen8858@gmail.com>
2024-02-08 18:13:59 +0800

    doughnut data map

--- components/doughnut-chart.js
+++ components/doughnut-chart.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import sourceData from "../data/sourceData.json";
 
 export default function DoughnutChart() {
   return (
-    <div>DoughnutChart</div>
-  )
+    <Doughnut
+      data={{
+        labels: sourceData.map((data) => data.label),
+        datasets: [
+          {
+            label: "Count",
+            data: sourceData.map((data) => data.value),
+            backgroundColor: [
+              "rgba(255, 166, 158, 0.8)",
+              "rgba(250, 192, 19, 0.8)",
+              "rgba(86, 213, 233, 0.8)",
+            ],
+            borderColor: [
+              "rgba(255, 166, 158, 0.8)",
+              "rgba(250, 192, 19, 0.8)",
+              "rgba(86, 213, 233, 0.8)",
+            ],
+          },
+        ],
+      }}
+    />
+  );
 }
